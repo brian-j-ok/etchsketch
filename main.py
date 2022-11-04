dcf182ba34fe804a7a957a36918922e6011fa33e
Brian O'Kelly <brianjrok@gmail.com>
2022-11-04 15:16:30 -0600

    created cursor controller

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello")
+from turtle import Turtle, Screen
+
+screen = Screen()
+cursor = Turtle()
+
+
+def move_forward():
+    cursor.forward(10)
+
+
+def move_backward():
+    cursor.back(10)
+
+def turn_left():
+    cursor.left(10)
+
+def turn_right():
+    cursor.right(10)
+
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.exitonclick()
